019dd1026c7a8389a1e69f5944a22f69269e98b4
kmsclass <kms@goodee.co.kr>
2025-06-04 15:55:27 +0900

    '20250604-1555'

--- shop1/src/main/java/dao/ItemDao.java
+++ shop1/src/main/java/dao/ItemDao.java
@@ -9,14 +9,31 @@ import org.springframework.stereotype.Repository;
 import dao.mapper.ItemMapper; //인터페이스로 생성
 import logic.Item;
 
-@Repository
+@Repository   //@Component + DAO관련기능. 객체화
 public class ItemDao {
-	@Autowired
+	@Autowired   //SqlSessionTemplate 객체 주입 
 	private SqlSessionTemplate template;
 	private Map<String, Object> param = new HashMap<>();
 	
 	private final Class<ItemMapper> cls = ItemMapper.class;
 	public List<Item> list() {
 		return template.getMapper(cls).select(null);  
+	}
+	public Item select(Integer id) {
+		param.clear();
+		param.put("id", id);
+		return template.getMapper(cls).select(param).get(0);  
+	}
+	public int maxId() {
+		return template.getMapper(cls).maxId();
+	}
+	public void insert(Item item) {
+		template.getMapper(cls).insert(item);
+	}
+	public void update(Item item) {
+		template.getMapper(cls).update(item);
+	}
+	public void delete(Integer id) {
+		template.getMapper(cls).delete(id);
 	}
 }
